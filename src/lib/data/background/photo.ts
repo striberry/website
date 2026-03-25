ffbe9f46d1eff3360cbd026b1918f3e52098a1c7
striberry <stribes@tutanota.de>
2026-03-25 18:27:59 +0100

    chore: format
    Signed-off-by: striberry <stribes@tutanota.de>

--- src/lib/data/background/photo.ts
+++ src/lib/data/background/photo.ts
@@ -18,66 +18,68 @@ export const photos: BackgroundImage[] = [
 	},
 	{
 		imageUrl: '/images/backgrounds/2.jpg',
-		workUrl: 'https://unsplash.com/photos/shallow-focus-photography-of-orange-leafed-tree-during-daytime-zrT1tjnxJKQ',
+		workUrl:
+			'https://unsplash.com/photos/shallow-focus-photography-of-orange-leafed-tree-during-daytime-zrT1tjnxJKQ',
 		artist: 'Lyndon Li',
 		artistUrl: 'https://unsplash.com/@dynastywind',
 		source: 'Unsplash',
 		sourceUrl: 'https://unsplash.com'
 	},
-  {
-    imageUrl: '/images/backgrounds/3.jpg',
-    workUrl: 'https://unsplash.com/photos/mountain-peak-s7W0bV2dUqQ',
-    artist: 'John Fowler',
-    artistUrl: 'https://unsplash.com/@wildhoney',
-    source: 'Unsplash',
-    sourceUrl: 'https://unsplash.com'
-  },
-  {
-    imageUrl: '/images/backgrounds/4.jpg',
-    workUrl: 'https://unsplash.com/photos/waterfall-surrounded-by-trees-YF4qTRQ2E28',
-    artist: 'Chad Madden',
-    artistUrl: 'https://unsplash.com/@chadmadden',
-    source: 'Unsplash',
-    sourceUrl: 'https://unsplash.com'
-  },
-  {
-    imageUrl: '/images/backgrounds/5.jpg',
-    workUrl: ' https://unsplash.com/photos/forest-pathway-GraajutbJHE',
-    artist: 'Elke Karin Lugert',
-    artistUrl: 'https://unsplash.com/@elke_karin',
-    source: 'Unsplash',
-    sourceUrl: 'https://unsplash.com'
-  },
-  {
-    imageUrl: '/images/backgrounds/6.jpg',
-    workUrl: 'https://unsplash.com/photos/landscape-photography-of-mountains-with-cloudy-skies-during-golden-hour-RbbdzZBKRDY',
-    artist: 'Nitish Meena',
-    artistUrl: 'https://unsplash.com/@nitishm',
-    source: 'Unsplash',
-    sourceUrl: 'https://unsplash.com'
-  },
-  {
-    imageUrl: '/images/backgrounds/7.jpg',
-    workUrl: 'https://unsplash.com/photos/landscape-photography-of-smoke-e4usjv5lmhE',
-    artist: 'Yosh Ginsu',
-    artistUrl: 'https://unsplash.com/@yoshginsu',
-    source: 'Unsplash',
-    sourceUrl: 'https://unsplash.com'
-  },
-  {
-    imageUrl: '/images/backgrounds/8.jpg',
-    workUrl: 'https://unsplash.com/photos/photo-of-mountain-at-sunset-sf9zGg5DnFw',
-    artist: 'Christina Gottardi',
-    artistUrl: 'https://unsplash.com/@cristina_gottardi',
-    source: 'Unsplash',
-    sourceUrl: 'https://unsplash.com'
-  },
-  {
-    imageUrl: '/images/backgrounds/9.jpg',
-    workUrl: 'https://unsplash.com/photos/great-wall-of-china-china-xrCQcLCE52o',
-    artist: 'Vincent Guth',
-    artistUrl: 'https://unsplash.com/@vingtcent',
-    source: 'Unsplash',
-    sourceUrl: 'https://unsplash.com'
-  }
+	{
+		imageUrl: '/images/backgrounds/3.jpg',
+		workUrl: 'https://unsplash.com/photos/mountain-peak-s7W0bV2dUqQ',
+		artist: 'John Fowler',
+		artistUrl: 'https://unsplash.com/@wildhoney',
+		source: 'Unsplash',
+		sourceUrl: 'https://unsplash.com'
+	},
+	{
+		imageUrl: '/images/backgrounds/4.jpg',
+		workUrl: 'https://unsplash.com/photos/waterfall-surrounded-by-trees-YF4qTRQ2E28',
+		artist: 'Chad Madden',
+		artistUrl: 'https://unsplash.com/@chadmadden',
+		source: 'Unsplash',
+		sourceUrl: 'https://unsplash.com'
+	},
+	{
+		imageUrl: '/images/backgrounds/5.jpg',
+		workUrl: ' https://unsplash.com/photos/forest-pathway-GraajutbJHE',
+		artist: 'Elke Karin Lugert',
+		artistUrl: 'https://unsplash.com/@elke_karin',
+		source: 'Unsplash',
+		sourceUrl: 'https://unsplash.com'
+	},
+	{
+		imageUrl: '/images/backgrounds/6.jpg',
+		workUrl:
+			'https://unsplash.com/photos/landscape-photography-of-mountains-with-cloudy-skies-during-golden-hour-RbbdzZBKRDY',
+		artist: 'Nitish Meena',
+		artistUrl: 'https://unsplash.com/@nitishm',
+		source: 'Unsplash',
+		sourceUrl: 'https://unsplash.com'
+	},
+	{
+		imageUrl: '/images/backgrounds/7.jpg',
+		workUrl: 'https://unsplash.com/photos/landscape-photography-of-smoke-e4usjv5lmhE',
+		artist: 'Yosh Ginsu',
+		artistUrl: 'https://unsplash.com/@yoshginsu',
+		source: 'Unsplash',
+		sourceUrl: 'https://unsplash.com'
+	},
+	{
+		imageUrl: '/images/backgrounds/8.jpg',
+		workUrl: 'https://unsplash.com/photos/photo-of-mountain-at-sunset-sf9zGg5DnFw',
+		artist: 'Christina Gottardi',
+		artistUrl: 'https://unsplash.com/@cristina_gottardi',
+		source: 'Unsplash',
+		sourceUrl: 'https://unsplash.com'
+	},
+	{
+		imageUrl: '/images/backgrounds/9.jpg',
+		workUrl: 'https://unsplash.com/photos/great-wall-of-china-china-xrCQcLCE52o',
+		artist: 'Vincent Guth',
+		artistUrl: 'https://unsplash.com/@vingtcent',
+		source: 'Unsplash',
+		sourceUrl: 'https://unsplash.com'
+	}
 ];
